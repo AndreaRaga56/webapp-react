92bd60745516c529ddb631488271326f3c31040d
AndreaRaga56 <andrearaga56@gmail.com>
2025-02-03 15:38:53 +0100

    feat select non funzionante

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -9,6 +9,11 @@ function MovieList() {
         <>
             <h1>Sei nella MovieList</h1>
             <div className="mt-4 card-container">
+                <label htmlFor="tagFiltro" className="form-label my-1 ">Filtra</label>
+                <select id="tagFiltro" name="tagFiltro" required className="form-select">
+                    <option value="" disabled>Seleziona...</option>
+
+                </select>
                 {
                     movies.map((curMovie, i) => {
                         return (
